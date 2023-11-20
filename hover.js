51c21f645230b9d8021f16f41afe1fa2c4e2dd55
mickel <mickel@gmailc.com>
2023-11-20 23:24:06 +0800

    2nd commit

---
+++ hover.js
@@ -0,0 +1,14 @@
+var img_1 = document.getElementById("img-1");
+
+
+function mouseIn(ele) {
+	if (ele == 1) {
+		img_1.setAttribute("src", "https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg");
+	}
+}
+
+function mouseOut(ele) {
+	if (ele == 1) {
+		img_1.setAttribute("src", "https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg");
+    }
+}
